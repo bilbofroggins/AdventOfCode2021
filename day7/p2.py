34b79dcaf9ee76b08b33b8918a023319c4c28985
Pat Cunniff <pjcfifa@gmail.com>
2021-12-07 00:45:49 -0500

    day7

--- day7/p2.py
+++ day7/p2.py
@@ -4,7 +4,7 @@ with open('input.txt') as f:
   	crabs = [int(x) for x in line.split(',')]
 
 best_position_low = int(math.floor(sum(crabs) / len(crabs)))
-best_position_high = int(round(sum(crabs) / len(crabs)))
+best_position_high = int(math.ceil(sum(crabs) / len(crabs)))
 
 fuel_low = 0
 for c in crabs:

9e7fd2d6a6ca2b05b5c4c65ddffc537c4b764c9e
Pat Cunniff <pjcfifa@gmail.com>
2021-12-07 00:43:50 -0500

    day7

--- day7/p2.py
+++ day7/p2.py
@@ -3,12 +3,18 @@ with open('input.txt') as f:
   for line in f:
   	crabs = [int(x) for x in line.split(',')]
 
-best_position = int(math.floor(sum(crabs) / len(crabs)))
+best_position_low = int(math.floor(sum(crabs) / len(crabs)))
+best_position_high = int(round(sum(crabs) / len(crabs)))
 
-fuel = 0
+fuel_low = 0
 for c in crabs:
-	diff = abs(c - best_position)
+	diff = abs(c - best_position_low)
 	triangular_number = int(diff*(diff+1)/2)
-	fuel += triangular_number
+	fuel_low += triangular_number
+fuel_high = 0
+for c in crabs:
+	diff = abs(c - best_position_high)
+	triangular_number = int(diff*(diff+1)/2)
+	fuel_high += triangular_number
 
-print(fuel)
+print(min(fuel_low, fuel_high))

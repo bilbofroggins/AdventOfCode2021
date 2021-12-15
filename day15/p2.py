29742f72c8d24ae9cc89220a021afe3bbfa73333
Pat Cunniff <pjcfifa@gmail.com>
2021-12-15 08:58:06 -0500

    day15

--- day15/p2.py
+++ day15/p2.py
@@ -1,33 +1,95 @@
-grid = []
+from heapq import heappush, heappop
+lines = []
 with open('input.txt') as f:
     for line in f:
-        grid.append([])
-        for num in line.strip():
-            grid[-1].append(int(num))
-starting = grid[0][0]
+        lines.append(line)
 
-og_rows = len(grid)
-og_cols = len(grid[0])
-bigger_grid = [[0]*(5*og_cols) for _ in range(5*og_rows)]
-for r in range(5*og_rows):
-    for c in range(5*og_cols):
-        bigger_grid[r][c] = grid[r % og_rows][c % og_cols] + int((r//og_rows) + (c//og_cols))
-        if bigger_grid[r][c] > 9:
-            bigger_grid[r][c] = bigger_grid[r][c] % 9
+def expandMap(xs):
+    t = [ [ (xs[r][c] + i + j - 1) % 9 + 1 for j in range(5) for c in range(len(xs)) ] for i in range(5) for r in range(len(xs)) ]
+    return t
 
-grid = bigger_grid
+def validCoord(xs, coord):
+    return coord[0] >= 0 and coord[0] < len(xs) and coord[1] >= 0 and coord[1] < len(xs[0])
 
-for row in range(len(grid) - 1, -1, -1):
-    for col in range(len(grid[0]) - 1, -1, -1):
-        if row == len(grid) - 1 and col == len(grid[0]) - 1:
-            min_val = grid[-1][-1]
-            continue
-        elif row == len(grid) - 1:
-            min_val = grid[row][col + 1]
-        elif col == len(grid[0]) - 1:
-            min_val = grid[row + 1][col]
-        else:
-            min_val = min(grid[row + 1][col], grid[row][col + 1])
-        grid[row][col] = grid[row][col] + min_val
-        
-print(grid[0][0] - starting)
+def gridGet(xs, coord):
+    return xs[coord[0]][coord[1]]
+
+def a_star(xs):
+    open = [ (-1, (0, 0)) ]
+    g = [ [ -1 for _ in range(len(xs[0])) ] for _ in range(len(xs)) ]
+    g[0][0] = 0
+    target = (len(xs)-1, len(xs[0])-1)
+    while len(open) > 0:
+        current = heappop(open)[1]
+        if current == target:
+            return gridGet(g, current)
+        for r, c in [ (1, 0), (0, 1), (-1, 0), (0, -1) ]:
+            adj = (current[0]+r, current[1]+c)
+            if validCoord(xs, adj):
+                oldG, newG = gridGet(g, adj), gridGet(g, current) + gridGet(xs, adj)
+                if newG < oldG or oldG == -1:
+                    g[adj[0]][adj[1]] = newG
+                    h = r + c
+                    if adj not in open:
+                        heappush(open, (newG + h, adj))
+
+xs = [ [ int(c) for c in line.strip() ] for line in lines ]
+
+print(a_star(expandMap(xs)))
+
+# from typing import List, Tuple
+
+# from heapq import heappush, heappop
+
+# def part1(xs: List[List[int]]) -> int:
+#     return a_star(xs)
+
+# def part2(xs) -> int:
+#     '''Bad runtime ~4s'''
+#     return a_star(expandMap(xs))
+
+# def a_star(xs: List[List[int]]):
+#     # https://en.wikipedia.org/wiki/A*_search_algorithm
+
+#     open = [ (-1, (0, 0)) ]
+#     g = [ [ -1 for _ in range(len(xs[0])) ] for _ in range(len(xs)) ]
+#     g[0][0] = 0
+#     target = (len(xs)-1, len(xs[0])-1)
+#     while len(open) > 0:
+#         current = heappop(open)[1]
+#         if current == target:
+#             print(g)
+#             return gridGet(g, current)
+#         for r, c in [ (1, 0), (0, 1), (-1, 0), (0, -1) ]:
+#             adj = (current[0]+r, current[1]+c)
+#             if validCoord(xs, adj):
+#                 oldG, newG = gridGet(g, adj), gridGet(g, current) + gridGet(xs, adj)
+#                 if newG < oldG or oldG == -1:
+#                     g[adj[0]][adj[1]] = newG
+#                     h = r + c  # heuristic, positive when moving towards target, negative otherwise
+#                     if adj not in open:
+#                         heappush(open, (newG + h, adj))
+#                         # print(open)
+#     return "failure"
+
+# def expandMap(xs: List[List[int]]) -> List[List[int]]:
+#     t = [ [ (xs[r][c] + i + j - 1) % 9 + 1 for j in range(5) for c in range(len(xs)) ] for i in range(5) for r in range(len(xs)) ]
+#     return t
+
+# def validCoord(xs: List[List[int]], coord: Tuple[int, int]) -> bool:
+#     return coord[0] >= 0 and coord[0] < len(xs) and coord[1] >= 0 and coord[1] < len(xs[0])
+
+# def gridGet(xs: List[List[int]], coord: Tuple[int, int]) -> int:
+#     return xs[coord[0]][coord[1]]
+
+# def main():
+#     lines = []
+#     with open('input.txt') as f:
+#         for line in f:
+#             lines.append(line)
+#     xs = [ [ int(c) for c in line.strip() ] for line in lines ]
+
+#     print("Part 2", part2(xs))
+
+# if __name__ == "__main__":
+#     main()
